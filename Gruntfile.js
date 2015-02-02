963b46134c8d2e66d3fc8d517e0cd85f15e18411
pkhruasu-ui <pkhruasu@gmail.com>
2015-02-01 16:40:32 -0800

    Add grunt concat/copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,14 +14,38 @@ module.exports = function(grunt){
 		},
 		sass : {
 			'stylesheets/compile.css' : ['stylesheets/sass/**/*.scss']			
+		},
+		concat : {
+			options : {
+				separator : ';\n',
+				banner : '//Creator : Prajak Khruasuwan\n\n'
+			},
+			dist : {								
+				files : {
+					'target/script.js' : ['scripts/*.js','scripts/**/*.js']	
+				}				
+			}
+		},
+		copy : {
+			main : {
+				files : [
+					{expand : true, src : ['index.html'], dest : 'target/'},
+					{expand : true, src : ['scripts/*.js','scripts/**/*.js'], dest : 'target/'},
+					{expand : true, src : ['stylesheets/*.css','stylesheets/**/*.css'], dest : 'target/'},
+					{expand : true, src : ['images/*'], dest : 'target/'},
+					{expand : true, src : ['node_modules/**'], dest : 'target/'},
+					{expand : true, src : ['views/*.html','views/**/*.html'], dest : 'target/'}					
+				]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('default',['jshint']);
-	grunt.registerTask('govern',['watch']);
-	grunt.registerTask('style',['sass']);
+	grunt.registerTask('default',['watch']);
+	// grunt.registerTask('style',['sass']);	
 };
